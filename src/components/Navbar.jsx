ca394380e7f11e22c3298647c00662d836685345
Brandon Harrison <50157560+BrandonHarrison2002@users.noreply.github.com>
2026-04-17 14:03:38 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ function Navbar() {
       <span className="navbar-name">Brandon Harrison</span>
       <div className="navbar-links">
         <a onClick={() => scrollTo("education")}>Education</a>
-        <a onClick={() => scrollTo("experience")}>Experience</a>
         <a onClick={() => scrollTo("skills")}>Skills</a>
         <a onClick={() => scrollTo("projects")}>Projects</a>
         <a onClick={() => scrollTo("contact")}>Contact</a>
