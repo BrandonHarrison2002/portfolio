3c263ae803fe2f66175ff8cecc103bfbafb6c07f
Brandon Harrison <50157560+BrandonHarrison2002@users.noreply.github.com>
2026-04-17 13:19:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Education from "./components/Education.jsx";
+import Certifications from "./components/Certifications.jsx";
+import Experience from "./components/Experience.jsx";
 import Skills from "./components/Skills.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <hr className="divider" />
-      <Skills />
-      <hr className="divider" />
-      <Projects />
-      <hr className="divider" />
-      <Contact />
-    </>
-  );
+    return (
+          <>
+                <Navbar />
+                <Hero />
+                <hr className="divider" />
+                <Education />
+                <hr className="divider" />
+                <Certifications />
+                <hr className="divider" />
+                <Experience />
+                <hr className="divider" />
+                <Skills />
+                <hr className="divider" />
+                <Projects />
+                <hr className="divider" />
+                <Contact />
+          </>>
+        );
 }
 
-export default App;
+export default App;</>
