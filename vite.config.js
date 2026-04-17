f6433887ec6445c9f6608eca51f2afa788f6bf89
Brandon Harrison <50157560+BrandonHarrison2002@users.noreply.github.com>
2026-04-17 12:30:43 -0400

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace "portfolio" below with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio/",
-  root: ".",
-  publicDir: "public",
 });
